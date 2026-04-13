de34b70966c11e31aafeedd891d25bda49d8da04
sf2012yk <sf2012yk@gmail.com>
2026-04-13 20:20:19 +0200

    Add files via upload

--- assets/js/icons/airport/airport.js
+++ assets/js/icons/airport/airport.js
@@ -33,9 +33,6 @@ className: "icon-airport"
     <div class="next_card" id="HeathrowToRomeCard">
     <p class="next_text">&#x2708; Rome（1400km）</span>
     </div>
-    <div class="next_card" id="HeathrowToDubaiCard">
-    <p class="next_text">&#x2708; Dubai（5000km）</span>
-    </div>
     <div class="next_card" id="HeathrowTerminal3ToPaddingtonCard">
     <p class="next_text">London City（23km）</span>
     </div>
@@ -152,42 +149,6 @@ var markerGenevaAirport = L.marker([46.2314339144919, 6.10746786781946], {
   </div>
 `;
 var markerViennaAirport = L.marker([48.11851237806887, 16.566266743122807], {
-  icon: AirportIcon 
-})
-  .bindPopup(popupContent, {
-  className: 'popup_wrapper', // ← ここで外側にクラスを追加
-  autoPan: true,
-  keepInView: true,
-})
-  .addTo(map);
-
-//  Dubai
-    var popupContent = `
-      <div class="popup_summer">
-    <a href="Dubai.html">
-      <img class="popup_img" loading="eager" src="../../assets/image/map/airport/dubai.webp" alt="Dubai Airport">
-    </a>
-    <div class="popup_caption">
-    <a href="Dubai.html" class="popup_link">Dubai Airport</a>
-    <p>
-    Dubaiの説明
-    </p>
-    </div>
-    <a onclick="toggleSection('nextVisitSection')" class="popup_link">▼ Next Airport</a>
-    <div id="nextVisitSection" class="popup_section">
-    <div class="next_card" id="DubaiToHeathrowCard">
-    <p class="next_text">&#x2708; London（6000km）</span>
-    </div>
-    <div class="next_card" id="DubaiToGeneveCard">
-    <p class="next_text">&#x2708; Geneve（5500km）</span>
-    </div>
-    <div class="next_card" id="DubaiToViennaCard">
-    <p class="next_text">&#x2708; Vienna（5000km）</span>
-    </div>
-    </div>
-  </div>
-`;
-var markerDubai = L.marker([25.24916816381139, 55.361520925952654], {
   icon: AirportIcon 
 })
   .bindPopup(popupContent, {
