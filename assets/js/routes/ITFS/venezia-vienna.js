063472087d7c8e229ec4c005bb025955bc8fb64d
sf2012yk <sf2012yk@gmail.com>
2026-05-31 16:21:50 +0200

    Add files via upload

--- assets/js/routes/ITFS/venezia-vienna.js
+++ assets/js/routes/ITFS/venezia-vienna.js
@@ -1,8 +1,132 @@
 // 鉄道ルート ベネチア⇔ウィーン　//
 const VeneziatoVienna = [45.44134690329801, 12.320284034940498];
+const MestretoVienna = [45.48203809284769, 12.231620771052933];
+const Treviso  = [45.659753269478394, 12.244947399883454];
+
+const Conegliano  = [45.88452331163624, 12.298804455543976];
+const Campodapallavolo  = [45.89602789574265, 12.328124581824692];
+const LagoVeneziane  = [45.90105821022709, 12.345224874108848];
+const Pianzano  = [45.913291904060465, 12.387985807493306];
+const Orsago_west  = [45.923320545963016, 12.422687614625758];
+const Orsago  = [45.92794945836864, 12.43420527360523];
+const Vistorta  = [45.941420934426645, 12.466775180205936];
+const Sacile  = [45.94835722055288, 12.498271316214376];
+const Pordenone  = [45.95649935183455, 12.653880885191196];
+const Cusano  = [45.945438907550326, 12.757081145506806];
+const Casarsa_west  = [45.95197924526397, 12.835493596314818];
+const Casarsa  = [45.95287435899797, 12.840407403131254];
+const Codroipo  = [45.96564820248356, 12.976218868683876];
+const Basiliano  = [46.012728035625564, 13.108954178092509];
+const VillaPrimavera  = [46.0474426679948, 13.207567693549256];
+const Udine  = [46.05570450990286, 13.242086807943766];
+const PmVAT  = [46.09289462701615, 13.247444972798045];
+const S_pelagio  = [46.15912899996613, 13.234881973446255];
+const Tarcento  = [46.20916212083072, 13.196184723705814];
+const Artegna  = [46.231165192036016, 13.162146335295727];
+const Gemona  = [46.27439083068804, 13.128140760291402];
+const Venzone  = [46.33201480277856, 13.135492389465108];
+const Carnia  = [46.37362066599693, 13.132678082750953];
+const Carnia_tunnel  = [46.39079529515746, 13.137202157293126];
+const Grauzaria  = [46.45025937548093, 13.187756432952582];
+const Pontebba_tunnel  = [46.497868099697286, 13.299001289805185];
+const Pontebba  = [46.50850222710019, 13.311596943295964];
+const UgovizzaValbruna  = [46.50275823275408, 13.495057471378884];
+const TarvisioBoscoverde  = [46.50634078742533, 13.607173355606934];
+const ThorlMaglern  = [46.540221101804406, 13.648172528412395];
+const Arnoldstein  = [46.55165710348015, 13.707475538554487];
+const Pockau  = [46.55177526855701, 13.746394442979145];
+const NeuhausGail   = [46.554974443362994, 13.778455740120526];
+const Furnitz  = [46.56343651785194, 13.81340794409453];
+const VillachWest  = [46.60807056140197, 13.840029610577682];
+const VillachHbf  = [46.61856520932838, 13.848483933362406];
+const Foderlach  = [46.602132233595846, 13.958945366761487];
+const LindRosegg  = [46.60600053399687, 14.000133702714995];
+const VeldenamWortherSee  = [46.61999470236589, 14.044545084113837];
+const Toschling  = [46.6331120844899, 14.111136063888738];
+const PortschachamWorthersee  = [46.63623983346714, 14.146110772277126];
+const Pritschitz  = [46.62803684031323, 14.170727559434898];
+const Krumpendorf  = [46.62647298561484, 14.221742559549543];
+const KlagenfurtWest  = [46.622928894844534, 14.266899342735972];
+const Klagenfurt  = [46.61577464702333, 14.31354753031239];
+const KlagenfurtEbenthal   = [46.61667767982707, 14.33385182718064];
+const BildstockChurch  = [46.62055543553211, 14.421240987069678];
+const Grafenstein  = [46.618998541603304, 14.459304726174622];
+const KuhnsdorfKlopeinerSee  = [46.624845896495266, 14.620732013756017];
+const Mittlern  = [46.59906208338179, 14.714367376461233];
+const Wiederndorf  = [46.62077752679473, 14.794777575580596];
+const StPaulimLavanttal  = [46.721412093649406, 14.85572845330239];
+const StPaul_tunnel  = [46.729862204074394, 14.861194565898769];
+const Weststeiermark  = [46.81395855939953, 15.296500541948877];
+const Wettmannstatten  = [46.83764240140857, 15.381660706342252];
+const Hengsberg  = [46.86350408782399, 15.457729911225497];
+const CargoCenterGraz  = [46.93533120082243, 15.46221357173681];
+const KalsdorfbeiGraz  = [46.983084202071005, 15.452778588602794];
+const FlughafenGraz  = [46.99728236640758, 15.44726958425993];
+const GrazSeiersberg  = [47.0126049154752, 15.440485177553521];
+const Puntigam  = [47.030764337113645, 15.429236402585179];
+const GrazDonBosco  = [47.05795763145009, 15.419223555131989];
+const GrazHbf  = [47.07278470866083, 15.416269910799851];
+const GratweinGratkorn  = [47.129207504263576, 15.323042448919866];
+const Stubing  = [47.17755244040638, 15.322474312402536];
+const PeggauDeutschfeistritz  = [47.20734300889064, 15.343176613973409];
+const Frohnleiten  = [47.26651452946225, 15.327107856993708];
+const Mixnitz  = [47.33137539260095, 15.360380071621465];
+const PerneggMur  = [47.359156298559405, 15.342978438804554];
+const BruckMur  = [47.41340904159284, 15.279444643139737];
+const Kapfenberg  = [47.44533296292859, 15.29248322934329];
+const Fachhochschule  = [47.454734371399724, 15.329286281910186];
+const SanktMareinimMurztal  = [47.47321679360965, 15.373184013380163];
+const Kindberg  = [47.5010936005553, 15.448168191175107];
+const WartbergimMurztal  = [47.527244256171144, 15.483739686632491];
+const MitterdorfimMurztal  = [47.536753639528406, 15.513213768413102];
+const Krieglach  = [47.547246688380426, 15.560167565939437];
+const Muerzzuschlag  = [47.608002816211346, 15.676982489778483];
+const SemmeringBasistunnel  = [47.67680512794481, 15.921965986547018];
+const Gloggnitz  = [47.677753662448815, 15.946459884904801];
+const Pottschach  = [47.6970919406472, 16.006238159101766];
+const Ternitz  = [47.7136118780588, 16.03317485665091];
+const Neunkirchen  = [47.731674815509734, 16.085180208546618];
+const Steinfeld  = [47.761019231344406, 16.14665614572747];
+const UnterwerkWrNeustadt  = [47.79854529832646, 16.22091658952541];
+const WienerNeustadt  = [47.81109567409707, 16.23356871558876];
+const WrNeustadtNord  = [47.83454636471038, 16.23861582677749];
+const Theresienfeld  = [47.8608048064508, 16.243999796780354];
+const Felixdorf  = [47.88760658732542, 16.247986886534594];
+const Sollenau  = [47.898530494030894, 16.245320573251174];
+const Leobersdorf  = [47.93841957794872, 16.231038325597208];
+const Kottingbrunn  = [47.95013216507507, 16.227084030908657];
+const BadVoslau  = [47.96929957057744, 16.223984229895287];
+const Baden_Vienna  = [48.00419598592161, 16.242845646401353];
+const Pfaffstatten  = [48.01841412427985, 16.259126092098917];
+const Gumpoldskirchen  = [48.04138558055613, 16.28539595794759];
+const Guntramsdorf  = [48.052119412284846, 16.297682943776902];
+const Modling  = [48.08573283877683, 16.295832828409218];
+const MariaEnzersdorf  = [48.10519058772991, 16.28804920712921];
+const Perchtoldsdorf  = [48.12291170691387, 16.28552604749889];
+const Liesing  = [48.135148217620404, 16.284285709937773];
+const Atzgersdorf  = [48.14652754115847, 16.288135850618737];
+const Hetzendorf  = [48.16685702434602, 16.31562138861379];
 const MeidlingfromVenezia = [48.174882700977655, 16.335360420988106];
 const ViennafromVenezia = [48.18500525586605, 16.377671700808875];
 
 // ベネチア⇔ウィーンのルート
 const FS_VeneziatoVienna = L.polyline
-([VeneziatoVienna,MeidlingfromVenezia,ViennafromVenezia], { color: '#000000' }).addTo(map);
+([VeneziatoVienna,MestretoVienna,
+    Treviso,
+    Conegliano,Campodapallavolo,LagoVeneziane,Pianzano,Orsago_west,Orsago,Vistorta,Sacile,
+    Pordenone,Cusano,Casarsa_west,Casarsa,Codroipo,Basiliano,VillaPrimavera,
+    Udine,PmVAT,S_pelagio,Tarcento,Artegna,Gemona,
+    Venzone,Carnia,Carnia_tunnel,Grauzaria,Pontebba_tunnel,Pontebba,UgovizzaValbruna,
+    TarvisioBoscoverde,ThorlMaglern,Arnoldstein,Pockau,NeuhausGail,Furnitz,VillachWest,
+    VillachHbf,Foderlach,LindRosegg,VeldenamWortherSee,Toschling,PortschachamWorthersee,Pritschitz,
+    Krumpendorf,KlagenfurtWest,
+    Klagenfurt,KlagenfurtEbenthal,BildstockChurch,Grafenstein,
+    KuhnsdorfKlopeinerSee,Mittlern,Wiederndorf,StPaulimLavanttal,StPaul_tunnel,Weststeiermark,Wettmannstatten,
+    Hengsberg,CargoCenterGraz,KalsdorfbeiGraz,FlughafenGraz,GrazSeiersberg,Puntigam,GrazDonBosco,
+    GrazHbf,GratweinGratkorn,Stubing,PeggauDeutschfeistritz,Frohnleiten,Mixnitz,PerneggMur,
+    BruckMur,Kapfenberg,Fachhochschule,SanktMareinimMurztal,Kindberg,WartbergimMurztal,MitterdorfimMurztal,
+    Krieglach,Muerzzuschlag,SemmeringBasistunnel,Gloggnitz,Pottschach,Ternitz,Neunkirchen,Steinfeld,UnterwerkWrNeustadt,
+    WienerNeustadt,WrNeustadtNord,Theresienfeld,Felixdorf,Sollenau,
+    Leobersdorf,Kottingbrunn,BadVoslau,Baden_Vienna,Pfaffstatten,Gumpoldskirchen,Guntramsdorf,
+    Modling,MariaEnzersdorf,Perchtoldsdorf,Liesing,Atzgersdorf,Hetzendorf,
+    MeidlingfromVenezia,ViennafromVenezia], { color: '#000000' }).addTo(map);
