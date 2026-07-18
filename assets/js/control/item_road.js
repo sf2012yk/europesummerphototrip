c97b1f1be6a51b6a0421e8241be74d326d1020a7
sf2012yk <sf2012yk@gmail.com>
2026-07-18 19:12:09 +0200

    Delete assets/js/control/item_road.js

--- assets/js/control/item_road.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('img').forEach(img => {
-  if (img.complete) {
-    img.classList.add('loaded');
-  } else {
-    img.addEventListener('load', () => img.classList.add('loaded'));
-  }
-});
