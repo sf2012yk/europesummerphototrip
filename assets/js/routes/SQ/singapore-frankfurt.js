b704e51f36add44f192f283de14f0bfc0a4d3f2f
sf2012yk <sf2012yk@gmail.com>
2026-09-13 18:31:10 +0200

    Add files via upload

--- assets/js/routes/SQ/singapore-frankfurt.js
+++ assets/js/routes/SQ/singapore-frankfurt.js
@@ -5,5 +5,5 @@ const FRA_Terminal1 = [50.05213795307499, 8.570923411132238];
 // ルートポリライン
 const SQ_Frankfurt = L.polyline
 ([Changi_FRA,FRA_Terminal1
-], { color: '#4d5252', weight: 1.5, opacity: 0.9, dashArray: '2, 4' }).addTo(map);
+], { color: '#0f1010', weight: 1.5, opacity: 0.9, dashArray: '2, 4' }).addTo(map);
      
--- assets/js/routes/SQ/singapore-london.js
+++ assets/js/routes/SQ/singapore-london.js
@@ -5,5 +5,5 @@ const LHR_Terminal3 = [51.47028371585865, -0.4590508896992913];
 // ルートポリライン
 const SQ_london = L.polyline
 ([Changi_LHR,LHR_Terminal3
-], { color: '#4d5252', weight: 1.5, opacity: 0.7, dashArray: '2, 4' }).addTo(map);
+], { color: '#0f1010', weight: 1.5, opacity: 0.7, dashArray: '2, 4' }).addTo(map);
      
