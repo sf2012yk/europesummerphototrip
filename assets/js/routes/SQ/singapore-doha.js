5d0b58e807407b1a20fb037a75e41227d91d6b8a
sf2012yk <sf2012yk@gmail.com>
2026-09-22 10:39:25 +0200

    Add files via upload

--- assets/js/routes/SQ/singapore-doha.js
+++ assets/js/routes/SQ/singapore-doha.js
@@ -1,9 +1,17 @@
 // 始発 終着 
-const Changi_DOH = [1.357498138269599, 103.98833398368646];
-const Doha_DOH = [25.267483125982892, 51.618988253293935];
+const QR_Changi_DOH = [1.357498138269599, 103.98833398368646];
+const QR_Tanamerah_DOH = [1.3106463450107246, 103.97653622583559];
+const QR_Malaka_DOH = [2.150551003794933, 102.23594047838777];
+const QR_Medan_DOH = [3.598739569237312, 98.68412772107796];
+const QR_Puducherry_DOH = [1.357498138269599, 103.98833398368646];
+const QR_Bengaluru_DOH = [13.04325435928171, 77.58843778362173];
+const QR_AbuDhabi_DOH = [24.432475375765843, 54.34460254122379];
+const QR_Doha_DOH = [25.267483125982892, 51.618988253293935];
 
 // ルートポリライン
-const SQ_Doha = L.polyline
-([Changi_DOH,Doha_DOH
+const QR_Doha = L.polyline
+([QR_Changi_DOH,QR_Tanamerah_DOH,QR_Malaka_DOH,QR_Medan_DOH,
+QR_Puducherry_DOH,QR_Bengaluru_DOH,QR_AbuDhabi_DOH,    
+QR_Doha_DOH
 ], { color: '#0f1010', weight: 1.5, opacity: 0.7, dashArray: '2, 4' }).addTo(map);
      
--- assets/js/routes/SQ/singapore-dubai.js
+++ assets/js/routes/SQ/singapore-dubai.js
@@ -1,9 +1,12 @@
 // 始発 終着 
-const Changi_DXB = [1.357498138269599, 103.98833398368646];
-const Dubai_DXB = [25.251227093649398, 55.36661696513273];
+const EM_Changi_DXB = [1.357498138269599, 103.98833398368646];
+const EM_Tanamerah_DXB = [1.3106463450107246, 103.97653622583559];
+const EM_KualaLumpur_DXB = [3.1957499853144187, 101.74054309289991];
+const EM_Nellore_DXB = [14.54929940875438, 80.0107592200021];
+const EM_Dubai_DXB = [25.251227093649398, 55.36661696513273];
 
 // ルートポリライン
-const SQ_Dubai = L.polyline
-([Changi_DXB,Dubai_DXB
+const EM_Dubai = L.polyline
+([EM_Changi_DXB,EM_Tanamerah_DXB,EM_KualaLumpur_DXB,EM_Nellore_DXB,EM_Dubai_DXB
 ], { color: '#0f1010', weight: 1.5, opacity: 0.7, dashArray: '2, 4' }).addTo(map);
      
